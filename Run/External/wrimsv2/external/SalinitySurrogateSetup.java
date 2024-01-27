c9d9547c6d7e744b0566202e403ab83fc0219197
Eli <Eli.Ateljevich@water.ca.gov>
2024-01-26 19:04:57 -0800

    Eliminated unused variable argument from ecsurrogate. Changed loading routine in anticipation of Rock Slough.

--- Run/External/wrimsv2/external/SalinitySurrogateSetup.java
+++ Run/External/wrimsv2/external/SalinitySurrogateSetup.java
@@ -34,26 +34,44 @@ public class SalinitySurrogateSetup{
 		int location = ssm.EMM_CALSIM;
 		DisaggregateMonths spline = new DisaggregateMonthsSpline(5);
 		DisaggregateMonths repeat = new DisaggregateMonthsRepeat(5);
-		DisaggregateMonths daysOps = new DisaggregateMonthsDaysToOps(5, 2., 0.);
+		DisaggregateMonths daysOps = new DisaggregateMonthsDaysToOps(5, 1., 0.);
         //TODO this 31 above is because of weird TF implementation
         // below was {spline, spline, daysOps but 3rd member daysOps changed to repeat
 		DisaggregateMonths[] disagg = { spline, spline, daysOps, spline, spline, spline, repeat };
-        Surrogate emm = emmatonANN();
+		String dir = "ann_calsim-main/emmaton";
+        Surrogate emm = loadSurrogate(dir);
         for (AggregateMonths agg : AggregateMonths.values()){
             int aggCode = agg.calsimCode;
-    		if (ssm.getSurrogateForSite(location, aggCode) == null){
+            System.out.println("Emm call location " + location + " agg code "+aggCode);
+    		if (! ssm.hasSurrogateForSite(location, aggCode)){
+                System.out.println("Loading Emmaton "+aggCode);
 			    SurrogateMonth month = new SurrogateMonth(disagg, emm, agg);
 			    ssm.setSurrogateForSite(location, aggCode, month);
                 ssm.setIndexForSite(location,0);   // 0 because univariate has only one index
             }
-        }			
+        }
+		//set up an ANN surrogate month for emmaton	
+		location = ssm.RSL_CALSIM;
+		//DisaggregateMonths[] disagg = { spline, spline, daysOps, spline, spline, spline, repeat };
+		dir = "ann_calsim-main/rock_slough";
+        Surrogate annRS = loadSurrogate(dir);
+        for (AggregateMonths agg : AggregateMonths.values()){
+            int aggCode = agg.calsimCode;
+            System.out.println("Rock Slough location " + location + " agg code "+aggCode);
+    		if (! ssm.hasSurrogateForSite(location, aggCode)){
+    			System.out.println("Loading Rockslough "+aggCode);
+			    SurrogateMonth monthRS = new SurrogateMonth(disagg, annRS, agg);
+			    ssm.setSurrogateForSite(location, aggCode, monthRS);
+                ssm.setIndexForSite(location,0);   // 0 because univariate has only one index
+            }
+        }	        
 		return ssm;
 
 	}
 
 
-	public static Surrogate emmatonANN() {
-		String fname = ExternalFunction.externalDir+ "ann_calsim-main/emmaton";
+	public static Surrogate loadSurrogate(String dir) {
+		String fname = ExternalFunction.externalDir+dir;
 		String[] tensorNames = { "serving_default_sac_input:0", "serving_default_exports_input:0",
 				"serving_default_dcc_input:0", "serving_default_net_dcd_input:0", "serving_default_sjr_input:0",
 				"serving_default_tide_input:0", "serving_default_smscg_input:0", };
