21df5b36a0450067ba65e32023f81d3c8ae3c2c4
Eli <Eli.Ateljevich@water.ca.gov>
2024-01-30 12:41:46 -0800

    Add NDOI to postprocessing script

--- Postprocess/plot_d1641_limiters.py
+++ Postprocess/plot_d1641_limiters.py
@@ -14,21 +14,23 @@ def plot_limiters(out_dir, out_file, title="D1641 Limiters"):
         "emm": "//EM_EC_MONTH/////",
         "jer": "//JP_EC_MONTH/////",
         "rsl": "//RS_EC_MONTH/////",
+        "ndoi": "//NDOI/FLOW/////"
     }
     std_paths = {
         "emm": "//EM_EC_STD/////",
         "jer": "//JP_EC_STD/////",
         "rsl": "//RS_EC_STD/////",
+        "ndoi": "//NDOI_MIN/FLOW/////"        
     }
 
-    names = {"emm": "Emmaton", "jer": "Jersey Point", "rsl": "Rock Slough"}
+    names = {"emm": "Emmaton", "jer": "Jersey Point", "rsl": "Rock Slough","ndoi":"NDOI"}
 
     fig, axes = plt.subplots(len(names), sharex=True, figsize=(7, 7))
 
     for ax, name in zip(axes, names.keys()):
         stds = list(pyhecdss.get_ts(fpath, std_paths[name]))
         std = stds[0].data
-        std = std.mask(std > 3000.0)
+        std = std.mask(std > 5000.0)
         monthly_ecs = list(pyhecdss.get_ts(fpath, monthly_paths[name]))  
         monthly_ec = monthly_ecs[0].data.shift(-1)
         label = names[name]
@@ -36,7 +38,12 @@ def plot_limiters(out_dir, out_file, title="D1641 Limiters"):
         monthly_ec.plot(ax=ax)
         ax.legend(["Standard","CalSim"])
         ax.set_title(names[name])
-        ax.set_ylabel(r"$\mu S/cm$")
+        if name == "ndoi":
+            label = "cfs"
+            ax.set_yscale("log",base=10)
+        else:
+            label = r"$\mu S/cm$"
+        ax.set_ylabel(label)
 
     fig.suptitle(title)
     plt.tight_layout()
